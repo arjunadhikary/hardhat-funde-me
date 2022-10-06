7c030ee0974573122cce66241670f4c492cd28e8
arjunadhikary <reasonworld12345@gmail.com>
2022-10-06 23:18:35 +0545

    added coverage package

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,17 +4,22 @@ require("dotenv").config()
 
 const GOERIL_ADDRESS = process.env.GOERIL_ADDRESS || ""
 const GOERIL_URL = process.env.GOERIL_URL || ""
+const ETHERSCAN_APIKEY = process.env.ETHERSCAN_APIKEY || ""
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     defaultNetwork: "hardhat",
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
     },
+    etherscan: {
+        apiKey: ETHERSCAN_APIKEY,
+    },
     networks: {
         goerli: {
             url: GOERIL_URL,
             accounts: [GOERIL_ADDRESS],
             chainId: 5,
+            blockConfirmation: 4,
         },
     },
     namedAccounts: {
